146f89bec39a2eb950cd77a8864926a4bd289a10
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2025-04-16 22:17:33 +0800

    update atcoreax431 fw

---
+++ keyboards/explorerkb/atcoreax431/config.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 @DreaM117er
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Main Keyboard Hand*/
+#define USE_SERIAL
+#define SERIAL_USE_MULTI_TRANSACTION
+#define MASTER_RIGHT
+
+/* Pointing Device Setting*/
+#define AZOTEQ_IQS5XX_TPS43
+//#define AZOTEQ_IQS5XX_ROTATION_180
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_RIGHT
+
+/* I2C or SPI Setting*/
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_PIN GP3
+#define I2C1_SDA_PIN GP2
---
+++ keyboards/explorerkb/atcoreax431/readme.md
@@ -0,0 +1,22 @@
+# Explorer Keyboard Blank #RGB
+
+![Blank](https://github.com/DreaM117er/Explorer-Keyboard-Blank/raw/main/pic/info/info4.jpg)
+
+The 3rd. Explorer Keyboard series, a Plank like split keyboard with muti-unit and 5V RGB LED Keylight.
+
+* Keyboard Maintainer: [DreaM117er](https://github.com/DreaM117er)
+* Keymap Supported: QMK, VIAL
+
+## Building the firmware
+
+Make example for this keyboard (after setting up your build environment):
+
+**RP2040 ProMicro supported: (RECOMMAND)**
+
+    make explorerkb/blankrgb/mcu/rp2040:[KEYMAP]
+
+or
+
+    qmk compile -c -kb explorerkb/blankrgb/mcu/rp2040 -km [KEYMAP]
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
